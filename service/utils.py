7dc8bef77357240791ffe8da763ec93f05940fcd
yasuomaidana <yasuomaidana@gmail.com>
2025-03-18 16:55:35 -0600

    Adding annotation to create entities with and without refresh

--- service/utils.py
+++ service/utils.py
@@ -15,3 +15,17 @@ def with_session(func):
             return result
         return func(self, *args, **kwargs)
     return wrapper
+
+
+def create_entity(func):
+    @wraps(func)
+    def wrapper(self, to_store: SQLModel, session: Session = None):
+        if session is None:
+            with Session(self.engine) as session:
+                session.add(to_store)
+                session.commit()
+                session.refresh(to_store)
+                session.close()
+            return to_store
+        func(self, to_store, session)
+    return wrapper
