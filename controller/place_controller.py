697dd6476dcb5081e12c73694d49b8d935742ef3
yasuomaidana <yasuomaidana@gmail.com>
2025-03-18 12:04:22 -0600

    Enhancing API documentation

--- controller/place_controller.py
+++ controller/place_controller.py
@@ -1,16 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from dto.create_place_dto import CreatePlaceDto
+from models import Place
 from service import PlaceService
 
-place_router = APIRouter(prefix="/place")
+place_router = APIRouter(prefix="/place", tags=["Place"])
 
-@place_router.post("")
+@place_router.post("", response_model=Place)
 async def create_place(place:CreatePlaceDto):
     place_service = PlaceService()
     return place_service.create_place(place)
 
-@place_router.get("/{place_id}")
+@place_router.get("/{place_id}", response_model=Place,responses={404: {"description": "Item not found"}})
 async def get_place(place_id: int):
     place_service = PlaceService()
+    place = place_service.get_place(place_id)
+    if not place:
+        raise HTTPException(status_code=404, detail="Item not found")
     return place_service.get_place(place_id)
