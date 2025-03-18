3d66f8ff36dda0cccbc2a22640266ab7aaf045d6
yasuomaidana <yasuomaidana@gmail.com>
2025-03-18 17:55:08 -0600

    Creating get places and delete places endpoints

--- controller/place_controller.py
+++ controller/place_controller.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, HTTPException
+from starlette.status import HTTP_204_NO_CONTENT
+
+# from starlette.status import HTTP_204_NO_CONTENT
 
 from dto.create_place_dto import CreatePlaceDto
 from models import Place
@@ -11,10 +14,24 @@ async def create_place(place:CreatePlaceDto):
     place_service = PlaceService()
     return place_service.create_from_dto(place)
 
-@place_router.get("/{place_id}", response_model=Place,responses={404: {"description": "Item not found"}})
+@place_router.get("")
+async def get_places():
+    place_service = PlaceService()
+    return place_service.get_places()
+
+@place_router.get("/{place_id}", response_model=Place,responses={404: {"description": "Place not found"}})
 async def get_place(place_id: int):
     place_service = PlaceService()
-    place = place_service.get_place(place_id)
+    place = place_service.get_place_by_id(place_id)
     if not place:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return place_service.get_place(place_id)
+        raise HTTPException(status_code=404, detail="Place not found")
+    return place_service.get_place_by_id(place_id)
+
+@place_router.delete("/{place_id}", status_code=HTTP_204_NO_CONTENT,
+                     responses={404: {"description": "Place not found"}, 204: {"description": "Item deleted"}})
+async def delete_place(place_id: int):
+    place_service = PlaceService()
+    place = place_service.get_place_by_id(place_id)
+    if not place:
+        raise HTTPException(status_code=404, detail="Place not found")
+    place_service.delete_place_by_id(place_id)
