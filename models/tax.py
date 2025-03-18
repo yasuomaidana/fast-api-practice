1909dcbc5935fbd2f5ee9e4ce2ff65e1fac84071
yasuomaidana <yasuomaidana@gmail.com>
2025-03-17 19:59:12 -0600

    Renaming taxes package to tax

--- models/tax.py
+++ models/tax.py
@@ -3,13 +3,15 @@ from typing import Optional
 
 from sqlmodel import SQLModel, Field
 
+
 class TaxType(Enum):
     VAT = "VAT"
     INCOME_TAX = "ISR"
-    
+
+
 class Tax(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
     tax_type: TaxType
     value: float
     invoiceId: Optional[int] = Field(default=TaxType.VAT, foreign_key="invoice.id")
-    
+   
