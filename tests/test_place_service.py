6218f77c7ede9f65ed7f7a4bf2c3befbffbde200
yasuomaidana <yasuomaidana@gmail.com>
2025-03-18 16:52:39 -0600

    Testing create place

---
+++ tests/test_place_service.py
@@ -0,0 +1,42 @@
+import os
+from unittest import TestCase, mock
+
+from sqlalchemy import create_engine
+from sqlmodel import SQLModel, Session
+
+from models import Place
+from models.place.place_type import PlaceType
+from service import PlaceService
+from settings.database_settings import DatabaseSettings
+
+
+class TestPlaceService(TestCase):
+    @mock.patch.dict(os.environ, {"DATABASE_NAME":":memory:"}, clear=True)
+    def setUp(self):
+        self.engine = create_engine(DatabaseSettings().dsn)
+        SQLModel.metadata.create_all(self.engine)
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("Engine disposed")
+    
+    def test__create_place(self):
+        
+        place_service = PlaceService(self.engine)
+        to_create = Place(name="test", place_type=PlaceType.STORE)
+        place_service._create_place(to_create)
+        self.assertIsNotNone(to_create.id)
+
+    def test__create_no_refresh(self):
+        place_service = PlaceService(self.engine)
+        with Session(self.engine) as session:
+            to_create = Place(name="test2", place_type=PlaceType.STORE)
+            place_service._create_place(to_create,session)
+            self.assertIsNone(to_create.id)
+            session.commit()
+            session.refresh(to_create)
+            self.assertIsNotNone(to_create.id)
+        
+    
+
+    
