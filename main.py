a8fdf2c37ffb0c5b11dc9fd41a37f5a098832873
yasuomaidana <yasuomaidana@gmail.com>
2025-03-17 23:07:34 -0600

    Cleaning main logic

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from controller.invoice_controller import invoice_router
+from controller.place_controller import place_router
+from settings.database_settings import create_db_and_tables
 
 app = FastAPI()
+app.include_router(invoice_router)
+app.include_router(place_router)
 
 
 @app.get("/")
@@ -8,16 +13,15 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
 @app.get("/wait")
 async def wait_message():
     import time
     time.sleep(10)
     return {"message": "I waited for 10 seconds"}
 
+
 if __name__ == "__main__":
     import uvicorn
+
+    create_db_and_tables()
     uvicorn.run(app, host="localhost", port=8000)
