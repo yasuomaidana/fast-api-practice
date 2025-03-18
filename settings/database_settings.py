7e19583e9b6f1a27e0f19d70ba2387759dc0e1c4
yasuomaidana <yasuomaidana@gmail.com>
2025-03-17 19:24:25 -0600

    fixing data model sqlite database name

--- settings/database_settings.py
+++ settings/database_settings.py
@@ -12,7 +12,8 @@ class DatabaseSettings(BaseSettings):
     @property
     def dsn(self):
         if self.type == "sqlite":
-            return f"{self.type}:///{self.host}"
+            name = self.name.replace("_",".") if "_db" in self.name else self.name+".db"
+            return f"{self.type}:///{name}"
         auth = f"{self.user}:{self.password}@" if self.user and self.password else ""
         return f"{self.type}://{auth}{self.host}:{self.port}/{self.name}"
     
