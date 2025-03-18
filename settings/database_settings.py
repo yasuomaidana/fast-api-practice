8d6c98b3ed039e2bdee777b6476a33bac2bb8893
yasuomaidana <yasuomaidana@gmail.com>
2025-03-18 16:43:19 -0600

    Enhancing database naming

--- settings/database_settings.py
+++ settings/database_settings.py
@@ -13,7 +13,7 @@ class DatabaseSettings(BaseSettings):
     @property
     def dsn(self):
         if self.type == "sqlite":
-            name = self.name.replace("_",".") if "_db" in self.name else self.name+".db"
+            name = self.name.replace("_",".") if "_db" in self.name else self.name
             return f"{self.type}:///{name}"
         auth = f"{self.user}:{self.password}@" if self.user and self.password else ""
         return f"{self.type}://{auth}{self.host}:{self.port}/{self.name}"
