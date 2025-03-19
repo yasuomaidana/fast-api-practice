b8793c35cde2b8a044828ff33c635785a928b67f
yasuomaidana <yasuomaidana@gmail.com>
2025-03-19 16:00:22 -0600

    Adding login endpoint

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -1,17 +1,32 @@
 from fastapi import APIRouter, HTTPException
 from sqlalchemy.exc import IntegrityError
-from starlette.status import HTTP_201_CREATED
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST
 
 from dto.user.create_user_dto import CreateUserDto
+from dto.user.login_dto import LoginDto
 from service.user_service import UserService
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
+user_service = UserService()
 
 
-@auth_router.post("/register", status_code=HTTP_201_CREATED, responses={400: {"description": "User already exists"}})
+@auth_router.post("/register", status_code=HTTP_201_CREATED,
+                  responses={HTTP_400_BAD_REQUEST: {"description": "User already exists"}})
 async def register(user: CreateUserDto):
-    user_service = UserService()
     try:
         user_service.register(user)
     except IntegrityError:
-        raise HTTPException(status_code=400, detail="Username or email already registered")
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Username or email already registered")
+
+
+@auth_router.post("/login", status_code=HTTP_200_OK,
+                  responses={HTTP_401_UNAUTHORIZED: {"description": "Invalid credentials"}})
+async def login(login_input: LoginDto):
+    user = user_service.find_account(login_input.username)
+    if not user:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Invalid username")
+    verified = user_service.auth_handler.verify_password(login_input.password, user.password)
+    if not verified:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    token = user_service.auth_handler.encode_token(user.id)
+    return {"token": token}
