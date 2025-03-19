03161657372dc8999b67e642546fa9b3387400dd
yasuomaidana <yasuomaidana@gmail.com>
2025-03-19 15:45:33 -0600

    Fixing refactoring to ensure it is receiving the proper engine

--- repository/place_repository.py
+++ repository/place_repository.py
@@ -2,16 +2,17 @@ from sqlalchemy import Engine
 from sqlmodel import Session, select
 
 from models import Place
-from repository.utils import create_entity, with_session
+from repository.utils import create_entity, with_session, transactional
+from settings import engine
 
 
 class PlaceRepository:
     _instance = None
 
-    def __new__(cls, engine: Engine):
+    def __new__(cls, _engine: Engine = engine):
         if cls._instance is None:
             cls._instance = super(PlaceRepository, cls).__new__(cls)
-            cls._instance.engine = engine
+            cls._instance.engine = _engine
         return cls._instance
 
     @create_entity
