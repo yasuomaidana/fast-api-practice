57c0eeab5f96a33861adfbb38c3e5eca7b19b2dd
yasuomaidana <yasuomaidana@gmail.com>
2025-03-19 17:21:09 -0600

    Using UTC for exp and iat, they must be on UTC to work with this library

--- auth/auth_payload.py
+++ auth/auth_payload.py
@@ -1,11 +1,18 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from settings.security import SecuritySettings
 
 
 class AuthPayload(BaseModel):
-    sub: int # user id
-    exp: datetime = datetime.now() + timedelta(seconds=SecuritySettings().expires_in) # expires in
-    iat: datetime = datetime.now() # issued at
+    sub: str  # user id # user id
+    """
+    Using UTC for JWT authentication ensures that the token's expiration and issuance times are consistent and 
+    not affected by time zone differences. 
+    This avoids potential issues with token validation across different time zones, 
+    ensuring that the tokens are valid and secure regardless of the user's location.
+    """
+    exp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc) + timedelta(
+        seconds=SecuritySettings().expires_in))  # expires in
+    iat: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))  # issued at
