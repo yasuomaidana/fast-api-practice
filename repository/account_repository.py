49fe0d1f4bd5bb3592f2e6e004f957d70c1cc1e9
yasuomaidana <yasuomaidana@gmail.com>
2025-03-19 15:46:47 -0600

    Creating account repository

---
+++ repository/account_repository.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Engine
+from sqlmodel import Session, select
+
+from models import Account
+from repository.utils import create_entity, with_session
+from settings import engine
+
+
+class AccountRepository:
+    _instance = None
+
+    def __new__(cls, _engine: Engine = engine):
+        if cls._instance is None:
+            cls._instance = super(AccountRepository, cls).__new__(cls)
+            cls._instance.engine = _engine
+        return cls._instance
+    
+    @create_entity
+    def create(self, account: Account, session: Session = None):
+        return session.add(account)
+    
+    @with_session
+    def find_by_id(self, account_id: int, session: Session = None):
+        return session.get(Account, account_id)
+    
+    @with_session
+    def find_by_username(self, username: str, session: Session = None):
+        return session.exec(select(Account).where(Account.username == username)).first()
+    
+    @with_session
+    def delete(self, account: Account, session: Session = None):
+        return session.delete(account)
