66df78ab2a9c6b3c42902f0710cabef0c43a6cae
yasuomaidana <yasuomaidana@gmail.com>
2025-03-21 11:55:04 -0600

    Setting Oauth url to /auth/oauth/token

--- settings/security/security_settings.py
+++ settings/security/security_settings.py
@@ -7,5 +7,6 @@ class SecuritySettings(BaseSettings):
     secret_key: str = "🤫it's a secret"
     algorithm: AlgorithmEnum = AlgorithmEnum.HS256
     expires_in: int = 3600  # seconds
+    token_url: str = "/auth/oauth/token"
 
     model_config = SettingsConfigDict(env_prefix='security_')
