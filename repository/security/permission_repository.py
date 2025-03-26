fab4273414a984c3ccbb0936394d9ab29d1a64cf
yasuomaidana <yasuomaidana@gmail.com>
2025-03-25 18:32:10 -0600

    Creating permission repository

---
+++ repository/security/permission_repository.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Engine
+from sqlmodel import Session, select
+
+from models import Permission
+from models.security.permission.permission_type import PermissionType
+from repository.utils import create_entity, with_session
+from settings import engine
+
+
+class PermissionRepository:
+    _instance = None
+    
+    def __new__(cls, _engine: Engine = engine):
+        if cls._instance is None:
+            cls._instance = super(PermissionRepository, cls).__new__(cls)
+            cls._instance.engine = _engine
+        return cls._instance
+    
+    
+    def create(self, permission: Permission | PermissionType, session: Session=None):
+        if isinstance(permission, PermissionType):
+            permission = Permission(permission=permission)
+        self._create(permission, session=session)
+        return permission
+    
+    @create_entity
+    def _create(self, permission: Permission, session: Session=None):
+        return session.add(permission)
+    
+    @with_session
+    def get(self, permission_type: PermissionType, session: Session=None):
+        statement = select(Permission).where(Permission.permission == permission_type)
+        return session.exec(statement).first()
+        
---
+++ tests/test_permission_repository.py
@@ -0,0 +1,45 @@
+import os
+from unittest import TestCase, mock
+
+from sqlmodel import SQLModel, Session, delete, create_engine
+
+from models import Permission
+from models.security.permission.permission_type import PermissionType
+from repository import PermissionRepository
+from settings import DatabaseSettings
+
+
+class TestPermissionRepository(TestCase):
+
+    @mock.patch.dict(os.environ, {"DATABASE_NAME": ":memory:"}, clear=True)
+    def setUp(self):
+        self.engine = create_engine(DatabaseSettings().dsn)
+        SQLModel.metadata.create_all(self.engine)
+        self.repository = PermissionRepository(self.engine)
+
+    def tearDown(self):
+        # Clean up the database
+        with Session(self.engine) as session:
+            statement = delete(Permission)
+            session.exec(statement)
+            session.commit()
+        session.commit()
+    
+    def test_create(self):
+        permission = Permission(permission=PermissionType.CREATE_INVOICE)
+        with Session(self.engine) as session:
+            self.repository.create(permission, session)
+            session.commit()
+            session.refresh(permission)
+            self.assertIsNotNone(permission.id)
+            self.assertEqual(permission.permission, PermissionType.CREATE_INVOICE)
+            
+    def test_create_annotated(self):
+        permission = self.repository.create(PermissionType.CREATE_INVOICE, session=None)
+        self.assertIsNotNone(permission.id)
+        
+    def test_get(self):
+        self.repository.create(Permission(permission=PermissionType.CREATE_INVOICE), session=None)
+        permission = self.repository.get(PermissionType.CREATE_INVOICE, session=None)
+        self.assertIsNotNone(permission)
+        
