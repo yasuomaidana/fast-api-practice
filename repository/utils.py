4f65dcd4fe0090d81b902905a45a059ef61902b5
yasuomaidana <yasuomaidana@gmail.com>
2025-03-26 13:28:03 -0600

    Adding documentation to the decorators

--- repository/utils.py
+++ repository/utils.py
@@ -4,6 +4,21 @@ from sqlmodel import Session, SQLModel
 
 
 def with_session(func):
+    """
+    Decorator to ensure a session is available for the decorated function.
+
+    This decorator applies the following behaviors:
+    - Checks if a session is provided in the function's keyword arguments.
+    - If no session is provided, it creates a new session using the engine.
+    - Passes the session to the decorated function.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: The wrapped function with the applied behaviors.
+    """
+
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         session = kwargs.get('session', None)
@@ -18,6 +33,20 @@ def with_session(func):
 
 
 def transactional(func):
+    """
+    Decorator to handle transactions in the database.
+
+    This decorator applies the following behaviors:
+    - Ensures a session is available using the `with_session` decorator.
+    - Commits the transaction after the function call.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: The wrapped function with the applied behaviors.
+    """
+
     @wraps(func)
     @with_session
     def wrapper(self, *args, **kwargs):
@@ -29,6 +58,20 @@ def transactional(func):
 
 
 def refreshable(func):
+    """
+    Decorator to refresh an entity after a function call.
+
+    This decorator applies the following behaviors:
+    - Ensures the entity is refreshed after the function call.
+    - Optionally refreshes specific attributes if `attribute_names` is provided.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: The wrapped function with the applied behaviors.
+    """
+
     @wraps(func)
     def wrapper(self, to_refresh: SQLModel, *args, attribute_names: list = None, **kwargs):
         func(self, to_refresh, *args, **kwargs)
@@ -39,6 +82,21 @@ def refreshable(func):
 
 
 def create_entity(func):
+    """
+    Decorator to handle the creation of an entity in the database.
+
+    This decorator applies the following behaviors:
+    - Ensures a session is available using the `with_session` decorator.
+    - Refreshes the entity after creation using the `refreshable` decorator.
+    - Commits the transaction using the `transactional` decorator.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: The wrapped function with the applied behaviors.
+    """
+
     @wraps(func)
     @with_session
     @refreshable
