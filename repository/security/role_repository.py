ab19b54fbd5e30f282f9fc27817eaf15688d41f3
yasuomaidana <yasuomaidana@gmail.com>
2025-03-25 18:02:50 -0600

    Creating role repository

---
+++ repository/security/role_repository.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Engine
+from sqlmodel import Session, select
+
+from models import Role
+from models.security.role.role_type import RoleType
+from repository.utils import create_entity, with_session
+from settings import engine
+
+
+class RoleRepository:
+    _instance = None
+    def __new__(cls, _engine: Engine = engine):
+        if cls._instance is None:
+            cls._instance = super(RoleRepository, cls).__new__(cls)
+            cls._instance.engine = _engine
+        return cls._instance
+    
+    @create_entity
+    def create(self, role: Role, session: Session = None):
+        return session.add(role)
+    
+    @with_session
+    def get(self, role_type: RoleType, session: Session = None):
+        statement = select(Role).where(Role.role == role_type)
+        return session.exec(statement).first()
---
+++ tests/test_role_repository.py
@@ -0,0 +1,46 @@
+import os
+from unittest import TestCase, mock
+
+from sqlmodel import SQLModel, Session, delete, create_engine
+
+from models import Role
+from models.security.role.role_type import RoleType
+from repository.security.role_repository import RoleRepository
+from settings import DatabaseSettings
+
+
+class TestRoleRepository(TestCase):
+    @mock.patch.dict(os.environ, {"DATABASE_NAME": ":memory:"}, clear=True)
+    def setUp(self):
+        self.engine = create_engine(DatabaseSettings().dsn)
+        SQLModel.metadata.create_all(self.engine)
+        self.repository = RoleRepository(self.engine)
+
+    def tearDown(self):
+        # Clean up the database
+        with Session(self.engine) as session:
+            statement = delete(Role)
+            session.exec(statement)
+            session.commit()
+        session.commit()
+        
+    def test_create(self):
+        role_type = RoleType.ADMIN
+        role = Role(role=role_type)
+        with Session(self.engine) as session:
+            self.repository.create(role, session)
+            session.commit()
+            session.refresh(role)
+            self.assertIsNotNone(role.id)
+            self.assertEqual(role.role, role_type)
+            
+    def test_create_annotated(self):
+        role_type = RoleType.ADMIN
+        role = Role(role=role_type)
+        self.repository.create(role, session=None)
+        self.assertIsNotNone(role.id)
+        
+    def test_get(self):
+        self.repository.create(Role(role=RoleType.ADMIN), session=None)
+        role = self.repository.get(RoleType.ADMIN, session=None)
+        self.assertIsNotNone(role)
