ae9392a4e6b7d3dce7e822a63a8048c352390dad
yasuomaidana <yasuomaidana@gmail.com>
2025-03-26 09:29:40 -0600

    Refactoring RolePermission, now it is link model

--- models/security/role_permision.py
+++ models/security/role_permision.py
@@ -1,14 +1,19 @@
 from sqlalchemy import UniqueConstraint
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field
 
 
 class RolePermission(SQLModel, table=True):
-    __tablename__ = "role_permission"
+    """
+    Represents the association between roles and permissions.
     
+    Attributes:
+        role_id (int): The ID of the role.
+        permission_id (int): The ID of the permission.
+    """
+    
+    __tablename__ = "role_permission"
+
     role_id: int = Field(foreign_key="role.id", primary_key=True)
     permission_id: int = Field(foreign_key="permission.id", primary_key=True)
 
     __table_args__ = (UniqueConstraint("role_id", "permission_id"),)
-
-    role: "Role" = Relationship(back_populates="permissions")
-    permission: "Permission" = Relationship(back_populates="roles")
