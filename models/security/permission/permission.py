5a0d98d1144e0b1214000ce35bf66c3edc96ccfd
yasuomaidana <yasuomaidana@gmail.com>
2025-03-25 18:32:24 -0600

    Adding documentation to permission

--- models/security/permission/permission.py
+++ models/security/permission/permission.py
@@ -6,6 +6,14 @@ from .permission_type import PermissionType
 
 
 class Permission(SQLModel, table=True):
+    """
+    Represents a permission in the system.
+
+    Attributes:
+        id (Optional[int]): The primary key of the permission.
+        permission (PermissionType): The type of permission.
+        roles (list["RolePermission"]): The roles associated with this permission.
+    """
     id: Optional[int] = Field(default=None, primary_key=True)
     permission: PermissionType
 
