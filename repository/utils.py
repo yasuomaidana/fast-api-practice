f21aa09d0fa0b1d1faa3bd750de7a42b6c504285
yasuomaidana <yasuomaidana@gmail.com>
2025-03-26 13:18:38 -0600

    Enhancing annotation by removing repeated code

--- repository/utils.py
+++ repository/utils.py
@@ -19,27 +19,31 @@ def with_session(func):
 
 def transactional(func):
     @wraps(func)
+    @with_session
     def wrapper(self, *args, **kwargs):
-        session = kwargs.get('session', None)
-        if session is None:
-            with Session(self.engine) as session:
-                kwargs['session'] = session
-                result = func(self, *args, **kwargs)
-                session.commit()
-            return result
+        result = func(self, *args, **kwargs)
+        kwargs['session'].commit()
+        return result
+
+    return wrapper
+
+
+def refreshable(func):
+    @wraps(func)
+    def wrapper(self, to_refresh: SQLModel, *args, attribute_names: list = None, **kwargs):
+        func(self, to_refresh, *args, **kwargs)
+        kwargs['session'].refresh(to_refresh, attribute_names=attribute_names)
+        return to_refresh
 
     return wrapper
 
 
 def create_entity(func):
     @wraps(func)
+    @with_session
+    @refreshable
+    @transactional
     def wrapper(self, to_store: SQLModel, session: Session = None):
-        if session is None:
-            with Session(self.engine) as session:
-                session.add(to_store)
-                session.commit()
-                session.refresh(to_store)
-            return to_store
-        func(self, to_store, session)
+        func(self, to_store, session=session)
 
     return wrapper
