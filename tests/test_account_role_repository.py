a861752edf97fe03a7e9fb4ad7a7acafb46c8ea5
yasuomaidana <yasuomaidana@gmail.com>
2025-03-26 17:47:37 -0600

    Testing account role repository

---
+++ tests/test_account_role_repository.py
@@ -0,0 +1,97 @@
+import os
+from unittest import TestCase, mock
+
+from sqlmodel import create_engine, SQLModel, Session, delete
+
+from models import Role, Permission, RolePermission, Account, AccountRole
+from models.security.permission.permission_type import PermissionType
+from models.security.role.role_type import RoleType
+from repository import RoleRepository, PermissionRepository, AccountRoleRepository
+from repository.account_repository import AccountRepository
+from repository.security.role_permission_repository import RolePermissionRepository
+from settings import DatabaseSettings
+
+
+class TestAccountRoleRepository(TestCase):
+
+    @classmethod
+    @mock.patch.dict(os.environ, {"DATABASE_NAME": ":memory:"}, clear=True)
+    def setUpClass(cls):
+        cls.engine = create_engine(DatabaseSettings().dsn)
+        SQLModel.metadata.create_all(cls.engine)
+        cls.role_repository = RoleRepository(cls.engine)
+        cls.permission_repository = PermissionRepository(cls.engine)
+        cls.role_permission_repository = RolePermissionRepository(cls.engine)
+        cls.account_role_repository = AccountRoleRepository(cls.engine)
+        cls.account_repository = AccountRepository(cls.engine)
+
+    def setUp(self):
+        self.role_repository.engine = self.engine
+        self.permission_repository.engine = self.engine
+        self.permission_repository.engine = self.engine
+        self.role_permission_repository.engine = self.engine
+        self.account_role_repository.engine = self.engine
+        self.account_repository.engine = self.engine
+
+        self.admin_role = self.role_repository.create(RoleType.ADMIN)
+        self.user_role = self.role_repository.create(RoleType.USER)
+
+        self.create_invoice_permission = self.permission_repository.create(PermissionType.CREATE_INVOICE)
+        self.delete_invoice_permission = self.permission_repository.create(PermissionType.DELETE_INVOICE)
+        self.read_invoice_permission = self.permission_repository.create(PermissionType.READ_INVOICE)
+        self.update_invoice_permission = self.permission_repository.create(PermissionType.UPDATE_INVOICE)
+
+        self.user1 = self.account_repository.create(Account(username="user1", password="password1", email="a@b.com"))
+        self.user2 = self.account_repository.create(Account(username="user2", password="password2", email="c@a.com"))
+
+        with Session(self.engine) as session:
+            admin_role = self.role_repository.get(RoleType.ADMIN, session=session)
+            user_role = self.role_repository.get(RoleType.USER, session=session)
+            admin_role.permissions = [self.create_invoice_permission, self.delete_invoice_permission,
+                                      self.read_invoice_permission,
+                                      self.update_invoice_permission]
+            user_role.permissions = [self.read_invoice_permission]
+            session.add(admin_role)
+            session.add(user_role)
+
+            session.commit()
+
+    def tearDown(self):
+        # Clean up the database
+        with Session(self.engine) as session:
+            for model in [Role, Permission, RolePermission, Account, AccountRole]:
+                session.exec(delete(model))
+            session.commit()
+
+    def test_create_user_with_single_role(self):
+        account_role = self.account_role_repository.create(self.user1, self.user_role)
+        self.assertEqual(len(account_role), 1)
+
+    def test_get_user_roles(self):
+        self.account_role_repository.create(self.user1, self.user_role)
+        with Session(self.engine) as session:
+            user_roles: Account = self.account_repository.find_by_id(self.user1.id, session=session)
+            self.assertEqual(len(user_roles.roles), 1)
+            session.commit()
+            session.refresh(user_roles, ["roles"])
+            self.assertEqual(len(user_roles.roles), 1)
+            self.assertEqual(user_roles.username, "user1")
+            self.assertTrue(user_roles.roles[0].enabled)
+
+    def test_create_without_repository(self):
+        user_1 = self.user1
+        roles = self.admin_role
+        with Session(self.engine) as session:
+            user_1 = self.account_repository.find_by_username(user_1.username, session=session)
+            roles = [AccountRole(account_id=user_1.id, role_id=roles.id)]
+            user_1.roles = roles
+            session.commit()
+            session.refresh(user_1, ["roles"])
+        self.assertEqual(len(user_1.roles), 1)
+    
+    def test_create_with_repository(self):
+        account_role = self.account_role_repository.create(self.user1, [self.user_role.id])
+        self.assertEqual(len(account_role), 1)
+        account_roles = self.account_role_repository.create(self.user2, [self.admin_role, self.user_role])
+        self.assertEqual(len(account_roles), 2)
+        
